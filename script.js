b4a4669db01e3ef5524e338abf0f11ae4c4b1b7c
R I T E S H <81812754+ritessshhh@users.noreply.github.com>
2024-02-10 18:48:12 -0500

    LoadingPage

--- script.js
+++ script.js
@@ -35,6 +35,10 @@
 //     });
 // });
 
+window.addEventListener('load', function () {
+    document.getElementById('loadingOverlay').style.display = 'none';
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     // Initialize an empty array to store the texts
     let data = [];
@@ -47,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
         item.addEventListener('click', function () {
             // Get the text content of the h4 element inside the clicked item
             const text = this.querySelector('h4').textContent;
-
+            document.getElementById('loadingOverlay').style.display = 'block';
             fetch('http://127.0.0.1:8000/start', {
                 method: 'POST',
                 body: text,
@@ -59,12 +63,14 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             }).then(data => {
                 // Set a value in sessionStorage
+                document.getElementById('loadingOverlay').style.display = 'none';
                 console.log(data);
-                
+
                 sessionStorage.setItem('topic', text)
                 sessionStorage.setItem('questionInfo', JSON.stringify(data));
                 window.location.assign("/game/game.html");
             }).catch(error => {
+                document.getElementById('loadingOverlay').style.display = 'none';
                 console.error('Error:', error);
             });
         });
