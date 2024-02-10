5a4402907ab0392ac10dc7dba8d54b13e22b6324
Sean Erfan <seanerfan@gmail.com>
2024-02-10 14:20:33 -0500

    Added animations

--- game/game.js
+++ game/game.js
@@ -16,25 +16,36 @@ let score = 0;
 const scoreText = document.getElementById("points")
 
 let card1 = document.getElementById("card1")
-card1 = { image: card1.querySelector("img"), title: card1.querySelector("div > h5"), description: card1.querySelector("div > p"), button: card1.querySelector("a") }
-
+card1 = { self: card1, image: card1.querySelector("img"), title: card1.querySelector("div > h5"), description: card1.querySelector("div > p"), button: card1.querySelector("a") }
 let card2 = document.getElementById("card2")
-card2 = { image: card2.querySelector("img"), title: card2.querySelector("div > h5"), description: card2.querySelector("div > p"), button: card2.querySelector("a") }
-
+card2 = { self: card2, image: card2.querySelector("img"), title: card2.querySelector("div > h5"), description: card2.querySelector("div > p"), button: card2.querySelector("a") }
 cards = [card1, card2]
+
+let correctCard;
+let incorrectCard;
 
 chooseQuestion();
 
 function answerQuestion(option) {
+    const newElement = document.createElement('dotlottie-player');
+    newElement.setAttribute('background', 'transparent');
+    newElement.setAttribute('speed', '1');
+    newElement.setAttribute('style', 'width: 300px; height: 300px;');
+    newElement.setAttribute('autoplay', '');
+
     if (option == correctAns) {
         score += 100
         console.log("Correct!");
-        scoreText.innerText = score;
+        // scoreText.innerText = score;
+        newElement.setAttribute('src', 'https://lottie.host/16cf6411-69cf-487e-84b9-56e7546e423a/83x9J6NMxd.json');
+        correctCard.self.appendChild(newElement)
+    
     }
     else {
         console.log("Incorrect!");
+        newElement.setAttribute('src', 'https://lottie.host/21d6933f-3fcc-48e4-9d95-7b66f5ec8e93/Xl94Qp2IpM.json');
+        incorrectCard.self.appendChild(newElement)
     }
-    chooseQuestion();
 }
 
 function chooseQuestion() {
@@ -42,12 +53,11 @@ function chooseQuestion() {
 
     correctAns = Math.floor(Math.random() * 2)
 
-    let incorrectCard = cards[1 - correctAns]
     correctCard = cards[correctAns];
-
     correctCard.image.src = candidates[questionInfo["correct_candidate"]].image
     correctCard.title.innerText = questionInfo["correct_candidate"]
 
+    incorrectCard = cards[1 - correctAns]
     incorrectCard.image.src = candidates[questionInfo["incorrect_candidate"]].image
     incorrectCard.title.innerText = questionInfo["incorrect_candidate"]
 }
