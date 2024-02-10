55232e006ffa0351db66c2856601bf8ada3c1524
R I T E S H <81812754+ritessshhh@users.noreply.github.com>
2024-02-10 02:53:45 -0500

    Javascript

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ selectElement.addEventListener("change", function () {
 const selectButton = document.getElementById("selectButton");
 
 selectButton.addEventListener("click", function () {
+    selectButton.className = "btn btn-success disabled";
+    selectButton.innerText = "Loading...";
     fetch('http://127.0.0.1:8000/start', {
         method: 'POST',
         body: selectedText,
@@ -21,8 +23,9 @@ selectButton.addEventListener("click", function () {
             throw new Error('Network response was not ok.');
         }
     }).then(data => {
-        console.log(data); // Log the response data
-        // Here you can handle the response data as needed
+
+        console.log(data);
+        window.location.assign("games/games.html");
     }).catch(error => {
         console.error('Error:', error);
     });
