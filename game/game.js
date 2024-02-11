50d692c58819875b601c668ebfdd296a6480fe2c
Sean Erfan <seanerfan@gmail.com>
2024-02-10 22:33:54 -0500

    Added descriptions and fixed redirecting from game to index

--- game/game.js
+++ game/game.js
@@ -8,15 +8,15 @@ let correctAns = undefined;
 console.log(questionInfo); // Output: testValue
 
 let candidates = {
-    "Joe Biden": { party: "democrat", image: "../images/Biden.png", description: "Joe Biden is here" },
-    "Donald Trump": { party: "republican", image: "../images/trump.png", description: "Donald Trump is here" },
-    "Nikki Haley": { party: "republican", image: "../images/haley.png", description: "Nicky Hailey is here" },
-    "Dean Phillips": { party: "democrat", image: "../images/philips.png", description: "Dean Phillips was here" }
+    "Joe Biden": { party: "democrat", image: "../images/Biden.png", description:      "A centrist Democrat with a focus on unity and bipartisanship, advocating for progressive policies while emphasizing collaboration across the political spectrum." },
+    "Donald Trump": { party: "republican", image: "../images/trump.png", description: "A populist Republican known for his nationalist and America-first agenda, often taking a confrontational approach to political adversaries and prioritizing issues like immigration reform and trade protectionism." },
+    "Nikki Haley": { party: "republican", image: "../images/haley.png", description:  "A conservative Republican with a pragmatic approach to foreign policy and a strong advocate for American exceptionalism and conservative values on the global stage." },
+    "Dean Phillips": { party: "democrat", image: "../images/philips.png", description:"A moderate Democrat committed to campaign finance reform and government accountability, prioritizing bipartisan solutions to address issues like healthcare and economic inequality." }
 }
 
 const progressBar = document.getElementById("progress");
 const progressBarMaxWidth = 340;
-const scoreIncrement = 30;
+const scoreIncrement = 300;
 
 let score = 0;
 progressBar.style.width = `${score}px`
@@ -46,7 +46,7 @@ function answerQuestion(option) {
     card2.button.disabled = true;
 
     if (option == correctAns) {
-        score += 30;
+        score += scoreIncrement;
         progressBar.style.width = `${score}px`
         console.log("Correct!");
         newElement.setAttribute('src', 'https://lottie.host/21d6933f-3fcc-48e4-9d95-7b66f5ec8e93/Xl94Qp2IpM.json');
@@ -66,26 +66,35 @@ function answerQuestion(option) {
             incorrectCard.self.removeChild(newElement);
         }, 2000);
     }
-    fetch('http://127.0.0.1:8000/start', {
-        method: 'POST',
-        body: topic,
-    }).then(response => {
-        if (response.ok) {
-            return response.json(); // Parse response body as JSON
-        } else {
-            throw new Error('Network response was not ok.');
-        }
-    }).then(data => {
-        // Set a value in sessionStorage
-        document.getElementById('loadingOverlay').style.display = 'none';
-        console.log(data);
-        questionInfo = data;
 
-        setTimeout(chooseQuestion, 3000);
-    }).catch(error => {
-        document.getElementById('loadingOverlay').style.display = 'none';
-        console.error('Error:', error);
-    });
+    if (score >= progressBarMaxWidth) {
+        sessionStorage.setItem('success', topic);
+        setTimeout(() => {
+            window.location.assign("/index.html");
+        }, 5000);
+    }
+    else {
+        fetch('http://127.0.0.1:8000/start', {
+            method: 'POST',
+            body: topic,
+        }).then(response => {
+            if (response.ok) {
+                return response.json(); // Parse response body as JSON
+            } else {
+                throw new Error('Network response was not ok.');
+            }
+        }).then(data => {
+            // Set a value in sessionStorage
+            document.getElementById('loadingOverlay').style.display = 'none';
+            console.log(data);
+            questionInfo = data;
+
+            setTimeout(chooseQuestion, 3000);
+        }).catch(error => {
+            document.getElementById('loadingOverlay').style.display = 'none';
+            console.error('Error:', error);
+        });
+    }
 }
 
 function chooseQuestion() {
@@ -100,11 +109,13 @@ function chooseQuestion() {
 
     correctCard = cards[correctAns];
     correctCard.image.src = candidates[questionInfo["correct_candidate"]].image
+    correctCard.description.innerText = candidates[questionInfo["correct_candidate"]].description
     correctCard.title.innerText = questionInfo["correct_candidate"]
     correctCard.self.classList.add(candidates[questionInfo["correct_candidate"]].party)
 
     incorrectCard = cards[1 - correctAns]
     incorrectCard.image.src = candidates[questionInfo["incorrect_candidate"]].image
+    incorrectCard.description.innerText = candidates[questionInfo["incorrect_candidate"]].description
     incorrectCard.title.innerText = questionInfo["incorrect_candidate"]
     incorrectCard.self.classList.add(candidates[questionInfo["incorrect_candidate"]].party)
 }
